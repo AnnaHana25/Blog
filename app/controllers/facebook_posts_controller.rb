11a485ffd5d7c0c892c46dfca891aa6c822de16b
AnnaHana25 <hunguga20005@gmail.com>
2024-02-16 11:05:55 +0200

    Final blog

--- app/controllers/facebook_posts_controller.rb
+++ app/controllers/facebook_posts_controller.rb
@@ -1,22 +1,16 @@
 class FacebookPostsController < ApplicationController
   def create
-    # Отримайте параметри, які прийшли з форми
     post_id = params[:post][:id]
     post = Post.find(post_id)
 
-    # Отримайте доступ до об'єкта користувача Koala
     user = Koala::Facebook::API.new(ENV['FACEBOOK_ACCESS_TOKEN'])
 
-    # Створіть повідомлення для публікації на Facebook
     message = "Новий пост на блозі: #{post.title}\n#{post.body}"
 
-    # Опціонально, вкажіть параметри, такі як зображення, посилання, тощо.
     options = { 'link' => post_url(post) }
 
-    # Опублікуйте повідомлення на стіні користувача
     user.put_wall_post(message, options)
 
-    # Опціонально, перенаправте користувача або виконайте інші дії після успішної публікації
     redirect_to post, notice: 'Повідомлення успішно опубліковано на Facebook.'
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -6,15 +6,6 @@ class Post < ApplicationRecord
   validates :body, presence: true
   validates :image, presence: true
 
-  after_create :publish_to_facebook
+  # after_create :publish_to_facebook
 
-  private
-
-  def publish_to_facebook
-    user = Koala::Facebook::API.new(ENV['FACEBOOK_ACCESS_TOKEN'])
-    message = "Новий пост на блозі: #{title}\n#{body}"
-    options = { 'link' => post_url(self) }
-
-    user.put_wall_post(message, options)
-  end
 end
