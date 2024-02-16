821d9a93d99b67c8c020b3ef17ba2aff62582ef5
AnnaHana25 <hunguga20005@gmail.com>
2024-02-16 16:34:43 +0200

    Final blog.2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!, unless: :devise_controller?
+
+  def sign_out_user
+    sign_out(current_user)
+    redirect_to root_path, notice: 'You have been signed out successfully.'
+  end
 end
